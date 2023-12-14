83bb75f799ec537072dbbcb6d8bbc9107743b78f
Mochen Yang <mochenyang@users.noreply.github.com>
2023-12-13 22:51:18 -0600

    added mongoDB capability

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-import os
 import time
 from openai import OpenAI
 from flask import Flask, render_template, request
+import pymongo
 
-client = OpenAI()
 app = Flask(__name__)
+
+# connect to mongodb
+dbclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = dbclient["TABot"]
+mycol = mydb["test"]
+user = "mochen"
+
+# connect to openai
+client = OpenAI()
 thread = client.beta.threads.create()
 
 def ask_assistant(user_input):
@@ -47,6 +55,9 @@ def home():
 def get_response():    
     userText = request.args.get('msg')
     response = ask_assistant(userText)
+    # record in database
+    record = { "user": user, "thread": thread.id, "time": time.time(), "query": userText, "response": response }
+    _ = mycol.insert_one(record)
     return response
 
 if __name__ == "__main__":
